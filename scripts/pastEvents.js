d5e58798ec599d2279d40a735e8a702409553163
SebastianCamacho <jscamacho@unicesar.edu.co>
2024-05-09 21:43:34 -0500

    SPRINT2 Terminado

---
+++ scripts/pastEvents.js
@@ -0,0 +1,276 @@
+let Datos = {
+    fechaActual: "2023-01-01",
+    eventos: [
+      {
+        _id: "639c723b992482e5f2834be9",
+        nombre: "Partido Colectividades",
+        imagen: "https://i.postimg.cc/Fs03hQDt/Colectivities-Party.jpg",
+        fecha: "2022-12-12",
+        descripción:
+          "Disfruta de tus platos favoritos, de diferentes países, en un evento único para toda la familia.",
+        categoría: "Feria de Alimentación",
+        lugar: "Sala A",
+        capacidad: 45000,
+        asistencia: 42756,
+        precio: 5,
+        __v: 0,
+      },
+      {
+        _id: "639c723b992482e5f2834beb",
+        nombre: "estilo coreano",
+        imagen: "https://i.postimg.cc/ZmD3Xf57/Korean-style.jpg",
+        fecha: "2023-08-12",
+        descripción:
+          "Disfruta de los mejores platos coreanos, con chefs internacionales y eventos increíbles.",
+        categoría: "Feria de Alimentación",
+        lugar: "Sala A",
+        capacidad: 45000,
+        precio: 10,
+        __v: 0,
+        estimación: 42756,
+      },
+      {
+        _id: "639c723c992482e5f2834bed",
+        nombre: "Parque Jurásico",
+        imagen: "https://i.postimg.cc/GmHRkbNV/Jurassic-Park.jpg",
+        fecha: "2022-11-02",
+        descripción:
+          "Vamos a conocer a los dinosaurios más grandes en el museo de paleontología.",
+        categoría: "Museo",
+        lugar: "Campo",
+        capacidad: 82000,
+        precio: 15,
+        __v: 0,
+        asistencia: 65892,
+      },
+      {
+        _id: "639c723c992482e5f2834bef",
+        nombre: "Museo Parisino",
+        imagen: "https://i.postimg.cc/c4C2zXm8/Parisian-Museum.jpg",
+        fecha: "2023-11-02",
+        descripción:
+          "Un recorrido único en la ciudad de la luz, conoce uno de los lugares más icónicos.",
+        categoría: "Museo",
+        lugar: "París",
+        capacidad: 8200,
+        estimación: 8200,
+        precio: 3500,
+        __v: 0,
+      },
+      {
+        _id: "639c723c992482e5f2834bf1",
+        nombre: "Comicón",
+        imagen: "https://i.postimg.cc/KYD0jMf2/comicon.jpg",
+        fecha: "2022-02-12",
+        descripción:
+          "Para los amantes del cómic, todos tus personajes favoritos reunidos en un solo lugar.",
+        categoría: "Fiesta de disfraces",
+        lugar: "Sala C",
+        capacidad: 120000,
+        asistencia: 110000,
+        precio: 54,
+        __v: 0,
+      },
+      {
+        _id: "639c723c992482e5f2834bf3",
+        nombre: "Noche de Halloween",
+        imagen: "https://i.postimg.cc/RZ9fH4Pr/halloween.jpg",
+        fecha: "2023-02-12",
+        descripción:
+          "Ven con tu disfraz más aterrador y gana premios increíbles.",
+        categoría: "Fiesta de disfraces",
+        lugar: "Sala C",
+        capacidad: 12000,
+        estimación: 9000,
+        precio: 12,
+        __v: 0,
+      },
+      {
+        _id: "639c723c992482e5f2834bf5",
+        nombre: "Metallica en concierto",
+        imagen: "https://i.postimg.cc/PrMJ0ZMc/Metallica-in-concert.jpg",
+        fecha: "2023-01-22",
+        descripción: "El único concierto de la banda más emblemática del mundo.",
+        categoría: "Concierto de música",
+        lugar: "Sala A",
+        capacidad: 138000,
+        estimación: 138000,
+        precio: 150,
+        __v: 0,
+      },
+      {
+        _id: "639c723c992482e5f2834bf7",
+        nombre: "Festival Electrónico",
+        imagen: "https://i.postimg.cc/KvsSK8cj/Electronic-Fest.jpg",
+        fecha: "2022-01-22",
+        descripción:
+          "Los mejores DJ's nacionales e internacionales reunidos en un solo lugar.",
+        categoría: "Concierto de música",
+        lugar: "Sala A",
+        capacidad: 138000,
+        asistencia: 110300,
+        precio: 250,
+        __v: 0,
+      },
+      {
+        _id: "639c723d992482e5f2834bf9",
+        nombre: "10K por la vida",
+        imagen: "https://i.postimg.cc/fyLqZY9K/10-K-for-life.jpg",
+        fecha: "2022-03-01",
+        descripción: "Ven y haz ejercicio, mejora tu salud y estilo de vida.",
+        categoría: "Carrera",
+        lugar: "Campo de fútbol",
+        capacidad: 30000,
+        asistencia: 25698,
+        precio: 3,
+        __v: 0,
+      },
+      {
+        _id: "639c723d992482e5f2834bfb",
+        nombre: "15K NY",
+        imagen: "https://i.postimg.cc/zv67r65z/15kny.jpg",
+        fecha: "2023-03-01",
+        descripción:
+          "Recaudaremos fondos para hospitales y atención médica en este evento único que se llevará a cabo en la Gran Manzana.",
+        categoría: "Carrera",
+        lugar: "Nueva York",
+        capacidad: 3000000,
+        precio: 3,
+        __v: 0,
+        estimación: 2569800,
+      },
+      {
+        _id: "639c723d992482e5f2834bfd",
+        nombre: "Feria del libro escolar",
+        imagen: "https://i.postimg.cc/Sst763n6/book1.jpg",
+        fecha: "2023-10-15",
+        descripción: "Trae tu libro escolar sin usar y toma el que necesitas.",
+        categoría: "Intercambio de libros",
+        lugar: "Sala D1",
+        capacidad: 150000,
+        estimación: 123286,
+        precio: 1,
+        __v: 0,
+      },
+      {
+        _id: "639c723d992482e5f2834bff",
+        nombre: "Solo para tu cocina",
+        imagen: "https://i.postimg.cc/05FhxHVK/book4.jpg",
+        fecha: "2022-11-09",
+        descripción:
+          "Si eres amante de la gastronomía ven a buscar el libro de cocina que mejor se adapte a tus gustos y al de tu familia.",
+        categoría: "Intercambio de libros",
+        lugar: "Sala D6",
+        capacidad: 130000,
+        asistencia: 90000,
+        precio: 100,
+        __v: 0,
+      },
+      {
+        _id: "639c723d992482e5f2834c01",
+        nombre: "Batman",
+        imagen: "https://i.postimg.cc/vH52y81C/cinema4.jpg",
+        fecha: "2022-3-11",
+        descripción: "Ven a ver a Batman luchar contra el crimen en Gotham City.",
+        categoría: "Cine",
+        lugar: "Sala D1",
+        capacidad: 11000,
+        asistencia: 9300,
+        precio: 225,
+        __v: 0,
+      },
+      {
+        _id: "639c723d992482e5f2834c03",
+        nombre: "Vengadores",
+        imagen: "https://i.postimg.cc/T3C92KTN/scale.jpg",
+        fecha: "2023-10-15",
+        descripción:
+          "Marvel's Avengers Premier en 3d, el inicio de una saga épica con tus superhéroes favoritos.",
+        categoría: "Cine",
+        lugar: "Sala D1",
+        capacidad: 9000,
+        estimación: 9000,
+        precio: 250,
+        __v: 0,
+      },
+    ],
+  };
+  
+function createCard(evento) {
+  // CREEAR LOS ELEMENTOS HTML
+  const divContTarjeta = document.createElement("div");
+  const card = document.createElement("div");
+  const image = document.createElement("img");
+  const cardBody = document.createElement("div");
+  const firstRow = document.createElement("div");
+  const Colinformation = document.createElement("div");
+  const cardTitleH5 = document.createElement("h5");
+  const cardTextP = document.createElement("p");
+  const secondRow = document.createElement("div");
+  const priceCol = document.createElement("div");
+  const priceElementH5 = document.createElement("h5");
+  const detailsCol = document.createElement("div");
+  const detailsButtonA = document.createElement("a");
+
+  // AÑADIR LAS CLASES A LOS ELEMENTOS
+  divContTarjeta.classList.add(
+    "col-10",
+    "col-sm-6",
+    "col-md-5",
+    "col-lg-4",
+    "col-xl-3",
+    "my-2"
+  );
+  card.classList.add("card", "h-100");
+  image.classList.add("card-img-top");
+  cardBody.classList.add("card-body");
+  firstRow.classList.add("row");
+  Colinformation.classList.add("col");
+  cardTitleH5.classList.add("card-title");
+  cardTextP.classList.add("card-text");
+  secondRow.classList.add("row", "mt-2");
+  priceCol.classList.add("col");
+  priceElementH5.textContent = "$5";
+  detailsCol.classList.add("col");
+  detailsButtonA.classList.add("btn");
+  detailsButtonA.textContent = "Details";
+  detailsButtonA.setAttribute("href", evento._id);
+
+  // Set image source and alt text
+  image.setAttribute("src", evento.imagen);
+  image.setAttribute("alt", "...");
+
+  divContTarjeta.appendChild(card);
+  card.appendChild(image);
+  card.appendChild(cardBody);
+  cardBody.appendChild(firstRow);
+  cardBody.appendChild(secondRow);
+  firstRow.appendChild(Colinformation);
+  secondRow.appendChild(priceCol);
+  secondRow.appendChild(detailsCol);
+  Colinformation.appendChild(cardTitleH5);
+  Colinformation.appendChild(cardTextP);
+  priceCol.appendChild(priceElementH5);
+  detailsCol.appendChild(detailsButtonA);
+
+  // ENVIAR DATOS
+  cardTitleH5.textContent = evento.nombre;
+  cardTextP.textContent = evento.descripción;
+  priceElementH5.textContent = "$" + evento.precio;
+
+  // Return the complete card element
+  return divContTarjeta;
+}
+
+const container = document.getElementById("contTarjetas1");
+
+const fechaActual = new Date(Datos.fechaActual);
+
+for (let index = 0; index < Datos.eventos.length; index++) {
+  const fechaEvento = new Date(Datos.eventos[index].fecha);
+  if (fechaActual.getTime() > fechaEvento.getTime()) {
+    const card1 = createCard(Datos.eventos[index]);
+    container.appendChild(card1);
+  }
+}
+console.log("Hola mundo past")
